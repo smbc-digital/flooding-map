b6fd1a338b0fe298cc60a50f898e259411c408bd
Dan Kinder <d.kinder02@gmail.com>
2020-07-21 14:26:01 +0100

    Popups fix

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -4,7 +4,7 @@ const flooding_testPopup = (feature, layer) => {
   <p></p>
   <p class="info">Enquiry Number: ${feature.properties.enquiry_number}</p>
   <p class="info">Type: ${feature.properties.status_name}</p>
-  <p> class="info">Type: ${feature.properties.job_entry_date}</p>
+  <p class="info">Date reported: ${feature.properties.job_entry_date}</p>
   
   </div>`
  
