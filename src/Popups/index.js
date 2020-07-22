56e343c0e8a40ce5deb40eb14e890c032978551b
Colin Lees <colinleesa7x@gmail.com>
2020-07-22 10:25:55 +0100

    fix: reported by date

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,10 +1,17 @@
-const floodingPopup = (feature, layer) => layer.bindPopup(
-  `<div class="item">
-    <i class="fa fa-map-signs" aria-hidden="true"></i>
-    <p class="title">Flooding Incident</p>
-    <p class="info">Enquiry Number: ${feature.properties.enquiry_number}</p>
-    <p class="info">Type: ${feature.properties.status_name}</p>
-    <p class="info">Reported Date: ${feature.properties.job_entry_date}</p>
-  </div>`)
+const floodingPopup = (feature, layer) => {
+  let noOfDays = Math.floor(
+    (new Date() - new Date(feature.properties.job_entry_date)) /
+      (1000 * 3600 * 24)
+  )
+
+  layer.bindPopup(
+    `<div class="item">
+      <i class="fa fa-map-signs" aria-hidden="true"></i>
+      <p class="title">Flooding Incident</p>
+      <p class="info">Enquiry Number: ${feature.properties.enquiry_number}</p>
+      <p class="info">Type: ${feature.properties.status_name}</p>
+      <p class="info">Flood reported ${noOfDays} days ago</p>
+    </div>`)
+  }
 
 export { floodingPopup }
